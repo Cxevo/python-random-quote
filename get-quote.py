fe28882416f926f5af51409b8a13ba62f2de4da2
Cxevo <ajpnfnwfe@gmail.com>
2021-11-30 22:31:41 -0500

    writes to file

--- get-quote.py
+++ get-quote.py
@@ -1,15 +1,22 @@
 import random
+import sys
 
 def main():
-  print("Keep it logically awesome.")
+  sys.stdout.write("Quote Generator. ")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
-  rnd = random.randint(0, last)
-
-  print(quotes[rnd])
+  f = open("quotes.txt", "a")
+  f.write('The best is yet to be. \n')
+  f.close()
+  last = 19
+  first = 1
+  rnd = random.randint(1, last)
+  rnd2 = random.randint(1, first)
+  
+  print(quotes[rnd], end='' )
+  sys.stdout.write(quotes[rnd2])
 
 if __name__== "__main__":
   main()
